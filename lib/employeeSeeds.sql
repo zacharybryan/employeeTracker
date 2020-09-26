118155c617157951e20099977a0cc4e1007b06c6
Zach Bryan <zacharybryan@icloud.com>
2020-09-26 04:36:06 -0700

    created employees table

--- lib/employeeSeeds.sql
+++ lib/employeeSeeds.sql
@@ -1,1 +1,10 @@
 CREATE SCHEMA `tesdf_db` ;
+
+CREATE TABLE `employee_db`.`employees` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(30) NOT NULL,
+    `last_name` VARCHAR(30) NOT NULL,
+    `role_id` INT NOT NULL,
+    `manager_id` INT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC));
