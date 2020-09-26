95f6f19f3e77f510f446e145ce572987e992c2dd
Zach Bryan <zacharybryan@icloud.com>
2020-09-26 04:29:22 -0700

    npm install, mysql connection made

--- tracker.js
+++ tracker.js
@@ -1,1 +1,20 @@
 var mysql = require('mysql');
+var inquire = require('inquire');
+
+const connectionConfig = {
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "employee_db"
+};
+
+var connection = mysql.createConnection(connectionConfig);
+
+connection.connect(function (err) {
+    if (err) {
+        console.error(`error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id  ${connection.threadId}`);
+});
