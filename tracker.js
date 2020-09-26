d406bd33903f30f3bae6c9fc6e8ba75c72914c1b
Zach Bryan <zacharybryan@icloud.com>
2020-09-26 05:11:06 -0700

    created add department function

--- tracker.js
+++ tracker.js
@@ -17,37 +17,48 @@ connection.connect(function (err) {
         return;
     }
     console.log(`connected as id  ${connection.threadId}`);
+    addDepartment();
 });
 
-function readRoster() {
-    
+function start() {
+    // inquire start up page with options that lead to functions
 };
 
 function addDepartment() {
-    
+    // (id, name)
+    connection.query(
+        'INSERT INTO department SET ?',
+        {
+            name: "Doctors"
+        },
+        function (error, data) {
+            if (error) throw error;
+            console.table("Doctors department added");
+
+        });
 };
 
 function addRole() {
-    
+    // (id, title, salary, department_id)
 };
 
 function addEmployee() {
-    
+    // (first_name, last_name, role_id, manager_id)
 };
 
-function viewDepartments(params) {
-    
+function viewDepartments() {
+    // prints departments to screen
 };
 
 function viewRoles() {
-    
+    // prints roles to screen
 };
 
 function viewEmployees() {
-    
+    // prints current roster to screen use console.table
 };
 
 
 function updateEmployeeRole() {
-    
+    // updates employee's role 
 };
